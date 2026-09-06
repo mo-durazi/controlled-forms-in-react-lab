b7b0daef02d2d71a17d7f1f409483fa49a821803
mo-durazi <mo.durazi.106@gmail.com>
2026-09-06 14:17:53 +0300

    finished Step 3 part a,-create handleInputChange-

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+
+    const [books, setBooks] = useState([{title: '', author: ''}]);
+
+    const [newBook, setNewBook] = useState({title: '', author: ''});
+
+    const handleInputChange = (evt) =>{
+        setNewBook({...newBook, [evt.target.name]: evt.target.value});
+    }
+
+
+
+    return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form action="">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" name="title" id="title" value={newBook.title} onChange={handleInputChange}/>
+
+                    <label htmlFor="author">Author: </label>
+                    <input type="text" name="author" id="author" value={newBook.author} onChange={handleInputChange}/>
+                
+                    <button type="submit">Add Book</button>
+                </form>
+            </div>
+            <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        </div>
+    )
+}
+
+export default Bookshelf;
